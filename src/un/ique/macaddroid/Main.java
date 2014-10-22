3f51ffe36ffa2acac1e0d94965bcb8e8c8f26b51
Matthew Finkel <Matthew.Finkel@gmail.com>
2014-10-22 20:35:51 +0000

    Retrieve and display MAC address

--- src/un/ique/macaddroid/Main.java
+++ src/un/ique/macaddroid/Main.java
@@ -2,6 +2,9 @@ package un.ique.macaddroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import un.ique.macaddroid.Layer2Address;
+import un.ique.macaddroid.NativeIOCtller;
+import android.widget.TextView;
 
 public class Main extends Activity
 {
@@ -11,5 +14,15 @@ public class Main extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Layer2Address newNet = new Layer2Address();
+        newNet.setInterfaceName("wlan0");
+        NativeIOCtller ctller = new NativeIOCtller(newNet);
+        newNet.setAddress(ctller.getCurrentMacAddr());
+        String addr = newNet.formatAddress();
+        TextView macField = (TextView)
+            findViewById(R.id.main_macaddress);
+        if (macField != null) {
+            macField.setText(addr);
+        }
     }
 }
