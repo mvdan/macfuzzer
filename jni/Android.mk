ae7cec888cb475db487361e29d64ab5acc9677c6
Matthew Finkel <Matthew.Finkel@gmail.com>
2014-10-23 14:01:35 +0000

    Compile it statically. It gets messy if we need to search for the lib.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,3 +5,18 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := native_ioctller
 LOCAL_SRC_FILES := native_ioctller.c
 include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := native_ioctller_static
+LOCAL_SRC_FILES := native_ioctller.c
+include $(BUILD_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := change_mac
+LOCAL_SRC_FILES := change_mac.c
+LOCAL_STATIC_LIBRARIES := native_ioctller_static
+include $(BUILD_EXECUTABLE)
