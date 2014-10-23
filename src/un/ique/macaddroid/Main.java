2aefe5478b607e1b77d2952a9046499255f2ccf1
Matthew Finkel <Matthew.Finkel@gmail.com>
2014-10-23 14:05:11 +0000

    Refresh the textview on return, so we display the new address

--- src/un/ique/macaddroid/Main.java
+++ src/un/ique/macaddroid/Main.java
@@ -29,6 +29,24 @@ public class Main extends Activity
         }
     }
 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Layer2Address newNet = new Layer2Address();
+        // Let's hardcode wlan0, for now
+        newNet.setInterfaceName("wlan0");
+        NativeIOCtller ctller = new NativeIOCtller(newNet);
+        newNet.setAddress(ctller.getCurrentMacAddr());
+        String addr = newNet.formatAddress();
+        TextView macField = (TextView)
+            findViewById(R.id.main_macaddress);
+        if (macField != null) {
+            macField.setText(addr);
+        }
+    }
+
+
     public void callRandomMac(View view)
     {
         Intent intent = new Intent(this, RandomMac.class);
