03b659d9cc41c428dec25a6f6d056de6ec1238b3
Matthew Finkel <Matthew.Finkel@gmail.com>
2014-11-27 05:45:39 +0000

    License MacAddroid under GPL version 3 or later
    Let us hope the G-ds are good to us and GPL stays true to its
    original intentions.

--- jni/change_mac.c
+++ jni/change_mac.c
@@ -1,3 +1,24 @@
+/*
+ * MacAddroid - Android app that changes a network devices MAC address
+ * Copyright (C) 2014 Matthew Finkel <Matthew.Finkel@gmail.com>
+ *
+ * This file is part of MacAddroid
+ *
+ * MacAddroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MacAddroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MacAddroid, in the COPYING file.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
